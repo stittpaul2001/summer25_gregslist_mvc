a1923f58cd5e15e047a3e88f50c5cf42fbf23220
Jeremy Fowler <jembyfowler@gmail.com>
2025-06-04 10:07:30 -0600

    drawing cars

--- app/controllers/CarsController.js
+++ app/controllers/CarsController.js
@@ -1,5 +1,17 @@
+import { AppState } from "../AppState.js";
+
 export class CarsController {
   constructor() {
     console.log('Cars controller is ready 🚓🚗🚙🏎️');
+    this.drawCars()
+  }
+
+  drawCars() {
+    const cars = AppState.cars
+    let carListingsContent = ''
+    cars.forEach(car => carListingsContent += car.listingHTMLTemplate)
+    const carListingsElem = document.getElementById('car-listings')
+    // console.log('drawing cars', carListingsElem);
+    carListingsElem.innerHTML = carListingsContent
   }
 }
